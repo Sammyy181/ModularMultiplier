7f86961e843cf979d4d70ac0414c423139351a1a
Sammyy181 <samarthg1804@gmail.com>
2025-09-12 13:08:31 +0530

    Pipelining Karatsuba

--- Verilog Files/karatsuba.v
+++ Verilog Files/karatsuba.v
@@ -21,152 +21,121 @@
 
 module karatsuba(
     input wire clock,
-    input wire [255:0] A,
-    input wire [255:0] B,
+    input wire in_valid,
+    input wire [255:0] X,
+    input wire [255:0] Y,
     input wire reset,
-    input wire start,
-    output reg [511:0] P,
-    output reg done
+    output reg [511:0] P
     );
     
-    reg [127:0] Phh, Phl, Plh, Pll; // Store Ahh * Bhh, Ahl * Bhl, Alh * Blh & All * Bll
-    reg [64:0] Ahsum, Bhsum, Alsum, Blsum; // Store partial sums of Ahh, Bhh, Ahl, Bhl, etc.
-	reg [128:0] Ph_sum, Pl_sum; // Store (Ahh*Bhh) + (Ahl*Bhl), same for Al and Bl 
-    reg [128:0] As, Bs; // Store Ah + Al, Bh + Bl
-    reg [129:0] Ps_H, Phsum, Plsum;
-    reg [127:0] Ps_L;
-    reg [65:0] As_sum, Bs_sum;
-    reg [130:0] midSumPart;
-    reg [129:0] Ph_mid, Pl_mid;
-    reg [131:0] Psum_mid, midSumTerm;
-    reg [131:0] tempP;
-    reg [129:0] Psumsums;
-    reg [131:0] tempP1, tempP2;
+    //1st Cycle 
+    reg [127:0] P00, P11, P22, P33;
+    reg [64:0] S10X, S10Y, S20X, S20Y, S21X, S21Y, S30X, S30Y, S31X, S31Y, S32X, S32Y;
+    reg valid_0;
     
-    reg [2:0] state;
+    //2nd Cycle
+    reg [128:0] P1P0, P2P0, P2P1, P3P0, P3P1, P3P2;
+    reg [129:0] S10, S20, S21, S30, S31, S32;
+    reg [127:0] T0K_1, P11_1, P22_1, T6K_1;
+    reg valid_1;
+    
+    //3rd Cycle
+    reg [128:0] T1K_2, T5K_2, M20, M21, M30, M31;
+    reg [127:0] T0K_2, P11_2, P22_2, T6K_2;
+    reg valid_2;
+    
+    //4th Cycle
+    reg [128:0] T2K_3, T4K_3;
+    reg [128:0] upSum_3;
+    reg [129:0] T3K_3;
+    reg [129:0] lowSum_3;
+    reg [63:0] result_3;
+    reg [63:0] T5K_L;
+    reg valid_3;
+     
     
     always @(posedge clock) begin
     	if(reset) begin
-    		done <= 1'b0;
     		P <= 512'b0;
-    		state <= 3'b000;
+    		valid_0 <= 1'b0;
+    		valid_1 <= 1'b0;
+    		valid_2 <= 1'b0;
+    		valid_3 <= 1'b0;
 		end
-   
-   		else begin
-   		case (state)
-   			
-   			3'b000: begin
-   				done <= 1'b0;
-   				if(start) begin
-   					// 1st Cycle
-   					Phh[127:0] <= A[255:192] * B[255:192];
-   					Phl[127:0] <= A[191:128] * B[191:128];
-   					Plh[127:0] <= A[127:64] * B[127:64];
-   					Pll[127:0] <= A[63:0] * B[63:0];
-   					
-   					As[128:0] <= A[127:0] + A[255:128];
-   					Bs[128:0] <= B[127:0] + B[255:128];
-   					Ahsum[64:0] <= A[255:192] + A[191:128];
-   					Alsum[64:0] <= A[127:64] + A[63:0];
-   					Bhsum[64:0] <= B[255:192] + B[191:128];
-   					Blsum[64:0] <= B[127:64] + B[63:0];
-   					state <= 3'b001;
-				end 
-				
-				else state <= 3'b000;
-			end
+    		
+    	else begin
+    		// 1st Cycle
+    		P00[127:0] <= X[63:0] * Y[63:0];
+    		P11[127:0] <= X[127:64] * Y[127*64];
+    		P22[127:0] <= X[191:128] * Y[191:128];
+    		P33[127:0] <= X[255:0] * Y[255:0];
+    		
+    		S10X <= X[127:64] + X[63:0];
+    		S20X <= X[191:128] + X[63:0];
+    		S30X <= X[255:192] + X[63:0];
+    		S21X <= X[191:128] + X[127:64];
+    		S31X <= X[255:192] + X[127:64];
+    		S32X <= X[255:192] + X[191:128];
+    		
+    		S10Y <= Y[127:64] + Y[63:0];
+			S20Y <= Y[191:128] + Y[63:0];
+			S30Y <= Y[255:192] + Y[63:0];
+			S21Y <= Y[191:128] + Y[127:64];
+			S31Y <= Y[255:192] + Y[127:64];
+			S32Y <= Y[255:192] + Y[191:128];
 			
-			3'b001: begin
-				// 2nd Cycle
-				
-				// Higher Term
-				Phsum[129:0] <= Ahsum[64:0] * Bhsum[64:0]; // Phsum
-				Ph_sum[128:0] <= Phh[127:0] + Phl[127:0]; // Phh + Phl
-				
-				
-				// Middle Term
-				Ps_H[129:0] <=  As[128:64] * Bs[128:64]; // A'h*B'h
-				Ps_L[127:0] <= As[63:0] * Bs[63:0]; // A'l * B'l
-				As_sum[65:0] <= As[128:64] + As[63:0]; // A'h + A'l
-				Bs_sum[65:0] <= Bs[128:64] + Bs[63:0]; // B'h + B'l
-				
-				//Lower Term
-				Plsum[129:0] <= Alsum[64:0] * Blsum[64:0]; // Plsum
-				Pl_sum[128:0] <= Plh[127:0] + Pll[127:0]; // Plh + Pll
-				
-				// Overall Term Arithmetic
-				P[511:0] <= {Phh[127:0], Phl[127:0], Plh[127:0], Pll[127:0]} - {128'b0, Phh[127:0], Phl[127:0], 128'b0}; 
-				
-				state <= 3'b010;
-			end 
-			
-			3'b010: begin
-				// 3rd Cycle
-				
-				// Higher Term
-				Ph_mid[129:0] <= Phsum[129:0] - Ph_sum[128:0];
-				
-				//  Middle Term
-				midSumPart[130:0] <= Ps_H[129:0] + Ps_L[127:0]; // P'h + P'l
-				Psum_mid[131:0] <= As_sum[65:0] * Bs_sum[65:0]; // (A'h + A'l)*(B'h + B'l)
-				
-				// Lower Term
-				Pl_mid[129:0] <= Plsum[129:0] - Pl_sum[128:0];
-				
-				// Overall Term Arithmetic
-				P[511:0] <= P[511:0] - {128'b0, Plh[127:0], Pll[127:0], 128'b0};
-				tempP[130:0] <= Phsum[129:0] + Plsum[129:0];
-				Psumsums[129:0] <= Ph_sum[128:0] + Pl_sum[128:0];
-				
-				state <= 3'b011; 
-			end
-				
-			3'b011: begin
-				//4th Cycle
-				
-				// Middle Term
-				midSumTerm[131:0] <= Psum_mid[131:0] - midSumPart[130:0];
-				
-				// Overall Term
-				P[511:0] <= P[511:0] + {62'b0, Ph_mid[129:0], 126'b0, Pl_mid[129:0] ,64'b0};
-				tempP[130:0] <= tempP[130:0] - Psumsums[129:0];
-				
-				state <= 3'b100;
-			end
-			
-			3'b100: begin
-				//5th Cycle
-				P[511:0] <= P[511:0] + (Ps_H[129:0] << 256);
-				//tempP[131:0] <= midSumTerm[131:0] - tempP[130:0];
-				tempP1[131:0] <= midSumTerm[131:0] - tempP[130:0];
-				tempP2[131:0] <= tempP[130:0] - midSumTerm[131:0];
-				state <= 3'b101;
-			end
-			
-			3'b101: begin
-				//6th Cycle
-				if(midSumTerm[131:0] > tempP[130:0]) 
-					P[511:0] <= P[511:0] + (tempP1 << 192);
-				else 
-					P[511:0] <= P[511:0] - (tempP2 << 192);
-				
-				state <= 3'b110;
-			end
-			
-			3'b110: begin
-				//7th Cycle (final)
-				P[511:0] <= P[511:0] + (Ps_L[127:0] << 128);
-				
-				done <= 1'b1;
-				state <= 3'b111;
-			end	
-			
-			3'b111: begin
-				done <= 1'b0;
-				state <= 3'b000;
-			end				
-		endcase 
-		end
-	end
+			valid_0 <= in_valid;
+    		
+    		// 2nd Cycle
+    		S10 <= S10X * S10Y;
+    		S20 <= S20X * S20Y;
+    		S21 <= S21X * S21Y;
+    		S30 <= S30X * S30Y;
+    		S31 <= S31X * S31Y;
+    		S32 <= S32X * S32Y;
+    		
+    		P1P0 <= P11 + P00;
+    		P2P0 <= P22 + P00;
+    		P2P1 <= P22 + P11;
+    		P3P0 <= P33 + P00;
+    		P3P1 <= P33 + P11;
+    		P3P2 <= P33 + P22;
+    		
+    		T0K_1 <= P00;
+    		P11_1 <= P11;
+    		P22_1 <= P22;
+    		T6K_1 <= P33;
+    		
+    		valid_1 <= valid_0;
+    		
+    		// 3rd Cycle
+    		T1K_2[128:0] <= S10[129:0] - P1P0[128:0];
+    		M20[128:0] <= S20[129:0] - P2P0[128:0];
+    		M21[128:0] <= S21[129:0] - P2P1[128:0];
+    		M30[128:0] <= S30[129:0] - P3P0[128:0];
+    		M31[128:0] <= S31[129:0] - P3P1[128:0];
+    		T5K_2[128:0] <= S32[129:0] - P3P2[128:0];
+    		
+    		T0K_2 <= T0K_1;
+    		P11_2 <= P11_1;
+    		P22_2 <= P22_1;
+    		T6K_2 <= T6K_1;
+    		
+    		valid_2 <= valid_1;
+    		
+    		// 4th Cycle
+    		result_3[63:0] <= T0K_2[63:0];
+    		lowSum_3[129:0] <= T1K_2[128:0] + T0K_2[127:64];
+    		T2K_3 <= P11_2 + M20;
+    		T3K_3 <= M30 + M21;
+    		T4K_3 <= P22_2 + M31;
+    		T5K_L[63:0] <= T5K_2[63:0];
+    		upSum_3[128:0] <= T5K_2[128:64] + T6K_2;
+    		
+    		valid_3 <= valid_2;
+    		
+    	end 
+	end 
+    		
 		
 endmodule 
